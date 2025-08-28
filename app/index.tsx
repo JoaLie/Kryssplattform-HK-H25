c3322a6bacb9af0bb1e8ecf9eb18006a38b94d79
naphoc <naphoc@gmail.com>
2025-08-28 15:43:20 +0200

    Fixed it

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,37 @@
 import Post from '@/components/Post';
 import { Stack } from 'expo-router';
 import { useState } from 'react';
-import { FlatList, Modal, Pressable, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
 
   const [isModalVisible, setIsModalVisible] = useState(false);
   // TODO: import posts from post.ts, for use in the FlatList
+  const posts = [
+    {
+      title: 'Mitt første innlegg',
+      description: 'Helt vanvittig mye tekst her',
+    },
+    {
+      title: 'Mitt andre innlegg',
+      description: 'Helt vanvittig mye tekst her også! Vittig a gitt',
+    },
+    
+    {
+      title: 'Mitt tredje innlegg',
+      description: 'Genialt Egon',
+    },
+    
+    {
+      title: 'Mitt fjerde innlegg',
+      description: 'Jättekul, eller hur?',
+    },
+    
+    {
+      title: 'Mitt femte innlegg',
+      description: 'Jeg er tom for ideer',
+    },
+  ];
 
   return (
     <View style={styles.mainContainer}>
@@ -17,14 +42,7 @@ export default function HomeScreen() {
       )
 
       }} />
-      <Modal
-      visible
-      >
-        <Pressable onPress={() => console.log('Knapp klikket')}>
-          <Text>Knapp?</Text>
-        </Pressable>
-      </Modal>
-      <FlatList 
+    <FlatList 
         data={posts}
         ItemSeparatorComponent={() => <View style={{height: 5}}></View>}
         renderItem={( post ) => 
